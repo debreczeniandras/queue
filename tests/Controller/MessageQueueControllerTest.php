5a98d8221d9667adfc54e44b0bf9fee43b816659
Andras Debreczeni <andras.debreczeni@db-n.com>
2020-07-03 16:00:35 +0200

    add test of the queue

---
+++ tests/Controller/MessageQueueControllerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @testdox Message Controller
+ */
+class MessageQueueControllerTest extends WebTestCase
+{
+    /**
+     * @testdox      Set options method returns a correct battle object
+     * @return string|null
+     */
+    public function testInserted(): string
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/v1/queue', [], [], ['CONTENT_TYPE' => 'application/json'],
+                         '{"priority": 1, "value": "Test Value"}');
+        
+        $response = $client->getResponse();
+        
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertTrue($response->headers->has('Location'));
+        
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $data);
+        $this->assertNotEmpty($data['id']);
+        $this->assertArrayHasKey('priority', $data);
+        $this->assertArrayHasKey('value', $data);
+        $this->assertEquals(1, $data['priority']);
+        $this->assertEquals('Test Value', $data['value']);
+        
+        return $response->headers->get('Location');
+    }
+    
+    /**
+     * @testdox      Set options method returns a correct battle object
+     * @return string|null
+     */
+    public function testIfPopWorks(): string
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/v1/queue', [], [], ['CONTENT_TYPE' => 'application/json'],
+                         '{"priority": 1, "value": "Test Value 1"}');
+        $client->request('POST', '/api/v1/queue', [], [], ['CONTENT_TYPE' => 'application/json'],
+                         '{"priority": 2, "value": "Test Value 2"}');
+        $client->request('POST', '/api/v1/queue', [], [], ['CONTENT_TYPE' => 'application/json'],
+                         '{"priority": 3, "value": "Test Value 3"}');
+        $client->request('POST', '/api/v1/queue', [], [], ['CONTENT_TYPE' => 'application/json'],
+                         '{"priority": 4, "value": "Test Value 4"}');
+        
+        $client->request('POST', '/api/v1/queue/messages');
+        $response = $client->getResponse();
+        
+        $this->assertEquals(200, $response->getStatusCode());
+        
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $data);
+        $this->assertNotEmpty($data['id']);
+        $this->assertArrayHasKey('priority', $data);
+        $this->assertArrayHasKey('value', $data);
+        $this->assertEquals(4, $data['priority']);
+        $this->assertEquals('Test Value 4', $data['value']);
+        
+        return $response->headers->get('Location');
+    }
+}
